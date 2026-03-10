d5ef27669fc84a670d2b305f70801b6b3ee59793
Paula Rodrigues <the.ipaulaa@gmail.com>
2026-03-10 11:36:30 -0300

    feat(exercise-04): intro to python

---
+++ 03-python/01-hello-world/04-initials/initials.py
@@ -0,0 +1,9 @@
+# Fun Fact: During my school years, I attended 10 different schools!
+
+print('PPPP     A    BBBB   RRRR')
+print('P   P   A A   B   B  R   R')
+print('P   P   A A   B   B  R   R')
+print('PPPP   AAAAA  BBBB   RRRR')
+print('P      A   A  B   B  R R')
+print('P      A   A  B   B  R  R')
+print('P      A   A  BBBB   R   R')
