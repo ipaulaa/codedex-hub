2537bcb90e6c6ef22fc025b4353a5bbc2603e329
Paula Rodrigues <the.ipaulaa@gmail.com>
2026-03-10 14:59:38 -0300

    refactor(exercise-05): simplify solution using multi-line strings

--- 03-python/01-hello-world/05-snail-mail/letter.py
+++ 03-python/01-hello-world/05-snail-mail/letter.py
@@ -1,24 +1,24 @@
-print("March 10, 2026")
-print()
-print("Dear Future Me 👑,")
-print("Quick checkpoint from the past.")
-print("I'm deep in Python 🐍, reviewing so I can return to tutoring 📚. My hurdles")
-print("at the moment are: structure, organization, and over‑planning 🤯. So, I guess")
-print("it's a good thing I'm feeling very motivated right now 🔥")
-print()
-print("My goals are clear: work remotely, contribute to open source, and building")
-print("useful code people will look up to 💡. And if you stayed committed, no")
-print("jumping ship halfway just because another shiny new course caught your")
-print("attention, I'm so proud of you. That's a HUGE win for us! ??")
-print()
-print("If you're reading this, it means you didn't quit 💪. You pushed through the")
-print("confusing parts and the frustrating bugs 🐛, and you kept learning and")
-print("building without giving up. I'm proud of you 🎉.")
-print()
-print("Now be honest with me...")
-print("Are we building cool things yet? 👀")
-print()
-print("Sincerely,")
-print("Past You 👧")
-print()
-print("P.S. Your top 3 emojis right now are: 🎉🥳🍀.")
+print("""March 10, 2026
+
+Dear Future Me 👑,
+Quick checkpoint from the past.
+I'm deep in Python 🐍, reviewing so I can return to tutoring 📚. My hurdles
+at the moment are: structure, organization, and over‑planning 🤯. So, I guess
+it's a good thing I'm feeling very motivated right now 🔥
+
+My goals are clear: work remotely, contribute to open source, and building
+useful code people will look up to 💡. And if you stayed committed, no
+jumping ship halfway just because another shiny new course caught your
+attention, I'm so proud of you. That's a HUGE win for us! 🏅
+
+If you're reading this, it means you didn't quit 💪. You pushed through the
+confusing parts and the frustrating bugs 🐛, and you kept learning and
+building without giving up. I'm proud of you 🎉.
+
+Now be honest with me...
+Are we building cool things yet? 👀
+
+Sincerely,
+Past You 👧
+
+P.S. Your top 3 emojis right now are: 🎉🥳🍀.""")
